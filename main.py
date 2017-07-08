7d5bda03e53e0e84f75eaf9f62e5e71c2fc43a79
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-07-09 05:15:09 +0700

    .

---
+++ main.py
@@ -0,0 +1,9 @@
+## -*- coding: utf-8 -*-.
+import telega.main as tel
+import config as settings
+import time
+
+Bot = tel.TelegramClass(settings.token,debug=True)
+#messages = Bot.getMessages()
+#Bot.JoinToChannel("i2p_ru")
+#Bot.sendMessage("i2p_ru","Test")
